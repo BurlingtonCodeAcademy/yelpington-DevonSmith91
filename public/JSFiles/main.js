50c27bf3e25c8a580d18b0b10d6170281c348de0
DevonSmith91 <DevonSmith91@gmail.com>
2020-03-09 23:47:03 -0400

    added comments

--- public/JSFiles/main.js
+++ public/JSFiles/main.js
@@ -1,3 +1,5 @@
+/* ------------------Random Functions-------------- */
+
 function capitalize(word) {
 	let steralize = word.toString().trim().toLowerCase()
 	let firstLetter = steralize[0].toUpperCase()
@@ -11,33 +13,29 @@ function titleize(sentence) {
 	return newArray.join(' ')
 }
 
-/* -------------------------Map Deets----------------------- */
-let myMap = L.map('mapid').setView([44.4759, -73.2121], 15);
-
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-	maxZoom: 19,
-	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(myMap)
-
 
 /* ----------------------Restaurant Deets------------------- */
 
 let listOfPlaces = document.getElementById('listOfPlaces')
 
 async function locationDetails() {
+	// Fetch to the custom API for restaurant list
 	fetch('./api/all.json')
 		.then((res) => {
 			return res.json()
 		})
 		.then((idList) => {
+			// Take each restaurant name and clean up the value, turn it into a list of links to their specific page
 			idList.forEach((id) => {
 				let dirtyName = id.split('-').join(' ')
 				let name = titleize(dirtyName)
 				listOfPlaces.innerHTML += `<p class='prettyText'><a class='sideLink' href='/restaurant?${id}'>${name}</a></p>`
+				// Use ID of restaurant name to fetch specific restaurant page
 				fetch(`./api/${id}.json`)
 					.then(res => res.json())
 					.then((restInfo) => {
 						let address = restInfo.address
+						// Fetch map information and plug in Lat/Lon into map
 						fetch(`https://nominatim.openstreetmap.org/search/?q=${address}&format=json`).then((data) => {
 							return data.json()
 						}).then((jsonObj) => {
@@ -55,4 +53,22 @@ async function locationDetails() {
 }
 
 locationDetails()
+
+
+
+/* -------------------------Map Deets----------------------- */
+
+//start the map looking at burlington
+
+let myMap = L.map('mapid').setView([44.4759, -73.2121], 15);
+
+
+// map tile details
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+	maxZoom: 19,
+	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(myMap)
+
+
 
--- public/JSFiles/restaurant.js
+++ public/JSFiles/restaurant.js
@@ -1,19 +1,11 @@
-/* ----------------------- Map deets ----------------------- */
+/* -------------------- Restaurant details ----------------- */
 
-
-let myMap = L.map('mapid').setView([44.4759, -73.2121], 15);
-
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(myMap)
-
-/* ---------------------- Restaurant deets ----------------- */
-
+// pull ID out of page name
 let url = window.location.search
 let urlArray = url.split('?')
 let id = urlArray.pop()
 
+// Breakdown of the detail sections
 let htmlName = document.getElementById('name')
 let htmlNotes = document.getElementById('notes')
 let htmlPhone = document.getElementById('phone')
@@ -22,50 +14,65 @@ let htmlHours = document.getElementById('hours')
 
 
 async function locationDetails() {
+    // fetch restaurant info from API
     let restaurantDetails = await fetch(`/api/${id}.json`)
         .then(res => res.json())
         .then((jsonObject) => {
             return jsonObject
         })
-
+    // Assign restaurant information to each section
     htmlName.textContent = restaurantDetails.name
-    htmlNotes.innerHTML = restaurantDetails.notes.join('<br /><br />')
+    htmlNotes.innerHTML = restaurantDetails.notes.join('<br/><br/>')
     htmlPhone.textContent = restaurantDetails.phone
     htmlAddress.textContent = restaurantDetails.address
     htmlHours.textContent = restaurantDetails.hours
 
+    // Fetch the address in nominatim to get Lat/Lon
     fetch(`https://nominatim.openstreetmap.org/search/?q=${restaurantDetails.address}&format=json`).then((data) => {
         return data.json()
     }).then((locInfo) => {
         let info = locInfo[0]
         let lat = info.lat
         let lon = info.lon
+        // Set Map view and drop pin
         myMap.setView([lat, lon], 18)
         L.marker([lat, lon]).addTo(myMap)
     })
-
 
 }
 
 locationDetails()
 
 
-// // saving local storage
+/* ---------------------- Map details ----------------------- */
+
+// Start map with a view of burlington
+let myMap = L.map('mapid').setView([44.4759, -73.2121], 15);
+
+// Map tile details
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(myMap)
+
+
+
+// -----------------------------------------------------------
+
+
+
+// Demo for saving to local storage. 
+
+
 
 // let commentForm = document.getElementById('comments')
 // let myStorage = window.localStorage
-
 // commentForm.addEventListener('submit', (event) => {
 //     let commentArray = myStorage.getItem('comments' + id) || []
-
 //     commentArray.push({ name: event.target.body.name, comment: event.target.body.comment })
-
 //     myStorage.getItems('comments' + id) ? myStorage.removeItem('comments' + id) : null
 //     myStorage.setItem('comments' + id, commentArray)
 //     commentArray.forEach((comment) => {
 //         document.getElementById('comment-display').innerHTML += `<p>${comment.name}:</p><p>${comment.comment}</p>`
-            
 //         })
-
-
 // })
